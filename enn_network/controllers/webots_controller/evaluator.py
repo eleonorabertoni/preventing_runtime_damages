d87c5f66959e128420dcad18b1f8e7b14bd8fea6
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2026-04-24 17:34:07 +0200

    fix: the evaluation function is modified to strongly penalize when the robot goes backward

--- enn_network/controllers/webots_controller/evaluator.py
+++ enn_network/controllers/webots_controller/evaluator.py
@@ -14,16 +14,12 @@ class Evaluator:
     # Evaluates the performance of the controller as the proximity from obstacles, direction, speed
     def update(self, inputs, vleft, vright):
         MAX_SPEED = 9.53
-        
-        max_value = 0
-        #max_index = -1
-        for i in range(len(inputs)):
-            value = inputs[i]
-            if max_value < value:
-                max_value = value
-                #max_index = i
 
-        V = (abs(vleft) + abs(vright)) / (2 * MAX_SPEED)
-        dV = (abs(vleft - vright)) / (2 * MAX_SPEED)
+        vleft = max(0, vleft) / MAX_SPEED
+        vright = max(0, vright) / MAX_SPEED
+
+        V = (abs(vleft) + abs(vright)) / 2
+        dV = abs(vleft - vright)
+        max_value = max(inputs)
 
         self.performance = V * (1 - math.sqrt(dV)) * (1 - max_value)
